0e667ba0ef2e675b2216d1d09f364a456f1c4525
Re-z <re-z@ukr.net>
2017-01-29 20:43:00 +0200

    generated list

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-var getted = fetch('arsenal.json');
-JSON.parse(getted);
+fetch('https://raw.githubusercontent.com/Re-z/js-practice/master/arsenal.json').then( function(response) {
+    return response.json()
+}).then(function (obj) {
+
+
+    var list = document.createElement('ul');
+    list.classList.add('list');
+    for (keys in obj) {
+        var item = document.createElement('li');
+        item.innerHTML = keys;
+        list.appendChild(item);
+    }
+    document.body.appendChild(list);
+
+
+
+
+
+})
